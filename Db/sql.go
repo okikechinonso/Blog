84cd838bee4eacb15ad181aba707496b181842e1
Okike Chinonso <Chinonsookike@gmail.com>
2022-01-26 00:39:48 +0100

    change from mysql to pg

--- Db/sql.go
+++ Db/sql.go
@@ -6,30 +6,45 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"log"
 )
-
+const (
+	host     = "ec2-3-227-15-75.compute-1.amazonaws.com"
+	port     = 5432
+	user     = "toxrdqthftqgwg"
+	password = "6047115930c93b6c21d63063e61de90b60ff428006ce854f83e532cb8384e758"
+	dbname   = "dd7680eila7962"
+  )
 
 
 func ConnectToDatabase() *sql.DB{
 	var db *sql.DB
-	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "",
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "CMS",
-	}
-	if cfg.User == "" || cfg.DBName == ""{
-		return nil
-	}
-	var err error
-	db, err = sql.Open("mysql", cfg.FormatDSN())
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println("conntected")
+
+  	fmt.Println("Successfully connected!")
+	
+	// if cfg.User == "" || cfg.DBName == ""{
+	// 	return nil
+	// }
+	// var err error
+	// db, err = sql.Open("postgres", cfg.FormatDSN())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// pingErr := db.Ping()
+	// if pingErr != nil {
+	// 	log.Fatal(pingErr)
+	// }
+	// fmt.Println("conntected")
 	return db
 }
